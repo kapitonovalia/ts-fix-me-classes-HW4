c65ef225818aad7d37e5d6f4f562f03fd856c7a8
kapitonovalia <aliiakapitonova@gmail.com>
2024-12-29 17:54:31 +0300

    fix2 person.ts file

--- src/person.ts
+++ src/person.ts
@@ -1,21 +1,21 @@
 class Person {
-    name: string
-    age: number
+    name: string;
+    age: number;
 
     constructor(name: string, age: number) {
-        this.name = name
-        this.age = age
+        this.name = name;
+        this.age = age;
     }
 
     getName(): string {
-        return this.name
+        return this.name;
     }
 
     getAge(): number {
-        return this.age
+        return this.age;
     }
 }
 
-const john = new Person('John', 30)
+const john = new Person('John', 30);
 console.log(john.getName()) // After fixing: will print "John"
 console.log(john.getAge())  // After fixing: will print 30
